3e624d119559e745e925b8d47d9b448da0366b75
Makiko <trimakichan@gmail.com>
2025-09-11 22:20:13 -0700

    complete wave 1 and pass test_wave_01

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,7 +1,62 @@
 from random import randint
 
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+
+
+def generate_shuffled_letter_list():
+    all_letters = []
+    shuffled_letters = []
+
+    for letter in LETTER_POOL:
+        qty_of_letter = LETTER_POOL[letter]
+        all_letters += letter * qty_of_letter
+
+    while all_letters:
+        random_index = randint(0,len(all_letters)-1)
+        shuffled_letters.append(all_letters[random_index])
+        all_letters.pop(random_index)
+
+    return shuffled_letters
+    
+
 def draw_letters():
-    pass
+    shuffled_letters = generate_shuffled_letter_list()
+    hand = []
+
+    for i in range(10):
+        random_index = randint(0,len(shuffled_letters)-1)
+        hand.append(shuffled_letters[random_index])
+        shuffled_letters.pop(random_index)
+    
+    return hand
+
 
 def uses_available_letters(word, letter_bank):
     pass
